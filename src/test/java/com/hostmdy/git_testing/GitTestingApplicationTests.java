dc1f0f1edc366a78bbd4e2e34a8a1a6dd86f919c
didii <didii@gmail.com>
2025-04-30 18:53:18 +0630

    master commit

---
+++ src/test/java/com/hostmdy/git_testing/GitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostmdy.git_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
